f1a341dd17e6786323aee64fc512f592863f43e2
PrastowoAdi <sinaadi75@gmail.com>
2024-02-05 00:35:36 +0700

    app_ci(prettier tailwind): setup prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='bg-black'>
-      <div className='flex'>
+      <div className='bg-black flex'>
         <h1 className=''>Halo</h1>
       </div>
     </main>
