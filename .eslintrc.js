74f5e53cc6fa589c3c494f55ed4935c863845660
PrastowoAdi <sinaadi75@gmail.com>
2024-02-05 00:10:06 +0700

    app_ci(init): init project

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  // This tells ESLint to load the config from the package `eslint-config-custom`
+  extends: ['custom', 'next/core-web-vitals'],
+  settings: {
+    next: {
+      rootDir: ['src/**/']
+    }
+  }
+}
