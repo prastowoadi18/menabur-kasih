4384b2d86367285fd613b67057f7b4841993d7a6
PrastowoAdi <sinaadi75@gmail.com>
2024-02-05 01:15:16 +0700

    app_ci(eslint): setup lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,20 @@ module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: ["custom", "prettier"],
+  rules: {
+    quotes: ["error", "single"],
+    "no-unused-vars": [
+      "error",
+      {
+        args: "after-used",
+        caughtErrors: "none",
+        ignoreRestSiblings: true,
+        vars: "all",
+      },
+    ],
+    "prefer-const": "error",
+    "react-hooks/exhaustive-deps": "error",
+  },
   settings: {
     next: {
       rootDir: ["src/**/"],
