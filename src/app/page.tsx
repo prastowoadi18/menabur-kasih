ac1d88e295159b66d6504228b9d7f5697cd69d1e
PrastowoAdi <sinaadi75@gmail.com>
2024-02-05 00:27:38 +0700

    app_fix(component): check prettier commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return <main>Hello</main>
+  return (
+    <main className='bg-black'>
+      <div className='flex'>
+        <h1 className=''>Halo</h1>
+      </div>
+    </main>
+  )
 }
